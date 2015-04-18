30adf4f7f833bc944d4bc16c4d49bd55aa592411
Jimmy Gong <gong.jim@gmail.com>
2015-04-18 15:39:28 -0700

    Adds mvp functionality

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,13 @@
+var jquery = {file: "lib/jquery.min.js"};
+var autofill = {file: "extension/actions/autofill.js"};
+
+/**
+ * Listen to keyboard shortcut to trigger autofill
+ */
 chrome.commands.onCommand.addListener(function(command) {
   if (command === 'fill-credentials'){
-    chrome.tabs.executeScript(null, {
-      file: "extension/actions/autofill.js"
+    chrome.tabs.executeScript(null, jquery, function(){
+      chrome.tabs.executeScript(null, autofill);
     });
   }
 });
--- extension/browser_action/popup.js
+++ extension/browser_action/popup.js
@@ -1,19 +1,21 @@
+var jquery = {file: "lib/jquery.min.js"};
+var autofill = {file: "extension/actions/autofill.js"};
+var redirect = {file: "extension/actions/redirect.js"};
+
 $(function(){
   /**
    * Redirect to extension site
    */
   $('#edit-template').click(function(){
-    chrome.tabs.executeScript(null, {
-      file: "extension/actions/redirect.js"
-    });
+    chrome.tabs.executeScript(null, redirect);
   });
 
   /**
    * Autofill current tab's form
    */
-  // $('#autofill').click(function(){
-  //   chrome.tabs.executeScript(null, {
-
-  //   });
-  // });
+  $('#autofill').click(function(){
+    chrome.tabs.executeScript(null, jquery, function(){
+      chrome.tabs.executeScript(null, autofill);
+    });
+  });
 });
