55e9e1a95d489995c73694ed4e31e72c02dd8466
Jimmy Gong <gong.jim@gmail.com>
2015-04-17 21:36:33 -0700

    Adds current tab interaction

--- extension/browser_action/popup.js
+++ extension/browser_action/popup.js
@@ -1,7 +1,21 @@
 $(function(){
   $('#edit-template').click(function(){
-
+    getCurrentTab(function(tab){
+      chrome.tabs.executeScript(tab.id ,{
+        code: "window.open('http://reddit.com')"
+        // code: "document.body.style.backgroundColor='red';"
+      });
+    });
   });
 });
 
 
+function getCurrentTab(callback) {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    var tab = tabs[0];
+    callback(tab);
+  });
+}
